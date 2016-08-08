e194c345e74d6745b93615b3a743fc147ed6cef4
Shahar Solomianik <shahar@gnift.com>
2016-08-08 21:53:30 +0300

    bah

--- app_modules/github.js
+++ app_modules/github.js
@@ -2,6 +2,8 @@ var config = require('config');
 var request = require('request');
 var _ = require('underscore');
 var util = require('util');
+var async = require('async')
+var parseLinkHeader = require('parse-link-header');
 
 module.exports = {
 	getAPIHeaders: function(accessToken){
@@ -11,4 +13,49 @@ module.exports = {
 			'User-Agent': config.get('app.name')
 		};
 	},
+	getUserRepos: function(accessToken,callback){
+		var headers = this.getAPIHeaders(accessToken);
+		var repos = [];
+		var page = 1;
+		var linkHeader;
+		
+		async.whilst(
+			function(){
+				return page;
+			},
+			function(callback){
+				request('https://api.github.com/user/repos?page=' + page,{headers: headers},function(error,response,body){
+					if(error){
+						callback(error);
+					}else if(response.statusCode > 300){
+						callback(response.statusCode + ' : ' + body);
+					}else{
+						var data = JSON.parse(body)
+						repos = repos.concat(data);
+						linkHeader = parseLinkHeader(response.headers.link);
+						page = (linkHeader? ('next' in linkHeader ? linkHeader.next.page : false) : false);
+						callback(null,repos);
+					}
+				});	
+			},
+			function(err,repos){
+				callback(err,repos)
+			}
+		);
+	},
+	getRepoPackage: function(accessToken,repo,callback){
+		var headers = this.getAPIHeaders(accessToken);
+		request('https://api.github.com/repos' + repo + '/contents/package.json',{headers: headers},function(error,response,body){
+			if(error){
+				callback(error);
+			}else if(response.statusCode > 300){
+				callback(response.statusCode + ' : ' + body);
+			}else{
+				var data = JSON.parse(body)
+console.log('package file for %s is: %s',repo.util.inspect(data))				
+				callback(null,data);
+			}
+		});	
+	}
+
 }
--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,40 @@ router.get('/', function(req, res, next) {
 	render(req,res,'index/index',{})
 });
 
+router.get('/explore', function(req, res, next) {
+	async.waterfall([
+		function(callback){
+			github.getUserRepos(req.session.user.github.access_token,function(err,repos){
+				callback(err,repos)
+			})
+		},	                 
+		function(repos,callback){
+			var packages = [];
+			async.each(repos,function(repo,callback){
+				github.getRepoPackage(req.session.user.github.access_token,repo.full_name,function(err,pkg){
+					if(err){
+						callback(err)
+					}else{
+						packages.push(pkg)
+						callback()
+					}
+				})
+			},function(err){
+				callback(err,packages)
+			})
+		},
+	],function(err,packages){
+		if(err){
+			errorHandler.error(req,res,next,err)
+		}else{
+			render(req,res,'index/explore',{
+				packages: packages
+			})
+		}
+	})
+	
+});
+
 function render(req,res,template,params){
 	
 	params.user = req.session.user;
